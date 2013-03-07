75200359ecbfcc58e4b6575ef7b76ed8be891d16
juanma.aguero@gmail.com <juanma.aguero@gmail.com>
2013-03-07 10:40:55 -0300

    Fixing the build

--- src/flowcode/wing/controller/DefaultController.php
+++ src/flowcode/wing/controller/DefaultController.php
@@ -2,15 +2,23 @@
 
 namespace flowcode\wing\controller;
 
+use flowcode\wing\mvc\Controller;
+use flowcode\wing\mvc\View;
+
 /**
  * Description of DefaultController
  *
  * @author juanma
  */
-class DefaultController {
+class DefaultController extends Controller {
+
+    function __construct() {
+        $this->setIsSecure(false);
+    }
 
     public function hello() {
-        echo "wing hello!";
+        $viewData["data"] = "handled by default controller.";
+        return View::getPlainView($this, "wing/view/default", $viewData);
     }
 
 }
