de0dfefa62496354fcea7ed1c74b268cd8d8c618
juanma.aguero@gmail.com <juanma.aguero@gmail.com>
2013-03-14 14:41:02 -0300

    Logger config, DefaultController tunne up

--- src/flowcode/wing/controller/DefaultController.php
+++ src/flowcode/wing/controller/DefaultController.php
@@ -2,15 +2,30 @@
 
 namespace flowcode\wing\controller;
 
+use flowcode\wing\mvc\Controller;
+use flowcode\wing\mvc\View;
+
 /**
  * Description of DefaultController
  *
  * @author juanma
  */
-class DefaultController {
+class DefaultController extends Controller {
+
+    function __construct() {
+        $this->setIsSecure(false);
+        $this->setName("defaultController");
+        $this->setModule("wing");
+    }
 
     public function hello() {
-        echo "wing hello!";
+        $viewData["data"] = "wing hello!";
+        return View::getPlainView($this, "wing/view/page/data-page", $viewData);
+    }
+
+    public function error() {
+        $viewData['data'] = "";
+        return View::getViewWithSpecificMaster($this, "wing/view/page/error-page", $viewData, "wing/view/master-static");
     }
 
 }
